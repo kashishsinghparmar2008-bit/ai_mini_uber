dfc204dd686a967370460c89fe36f1c28f4420c1
kashishsinghparmar2008-bit <kashishsinghparmar2008@gmail.com>
2026-01-04 22:36:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,34 @@
-// Mini Uber Backend
-// Basic server starter file
+const express = require("express");
+const dijkstra = require("./graph");
+const drivers = require("./drivers");
 
-console.log("Mini Uber backend is running");
+const app = express();
+
+// Test route
+app.get("/ride", (req, res) => {
+  const pickup = "D"; // passenger pickup point
+
+  let nearestDriver = null;
+  let minDistance = Infinity;
+
+  // Find nearest driver
+  for (let driver of drivers) {
+    const result = dijkstra(driver.location, pickup);
+
+    if (result.distance < minDistance) {
+      minDistance = result.distance;
+      nearestDriver = driver;
+    }
+  }
+
+  res.json({
+    pickupPoint: pickup,
+    assignedDriver: nearestDriver.name,
+    driverLocation: nearestDriver.location,
+    distanceToPickup: minDistance
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Mini Uber server running on port 3000");
+});
