ff48abedfbf416a4c1db36a60302695c4bf5ca9b
kashishsinghparmar2008-bit <kashishsinghparmar2008@gmail.com>
2026-01-04 22:33:31 +0530

    Update drivers.js

--- backend/drivers.js
+++ backend/drivers.js
@@ -1,1 +1,7 @@
+const drivers = [
+  { id: 1, name: "Driver 1", location: "B" },
+  { id: 2, name: "Driver 2", location: "C" },
+  { id: 3, name: "Driver 3", location: "A" }
+];
 
+module.exports = drivers;
