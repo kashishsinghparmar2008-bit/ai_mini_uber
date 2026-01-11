a0a40a185cba751ad13d8fa9a39ebe91b42c2cf4
kashishsinghparmar2008-bit <kashishsinghparmar2008@gmail.com>
2026-01-11 23:49:22 +0530

    Update fareModel.js

--- backend/fareModel.js
+++ backend/fareModel.js
@@ -1,1 +1,27 @@
+// backend/fareModel.js
 
+const pastRides = [
+  { distance: 5, fare: 120 },
+  { distance: 8, fare: 180 },
+  { distance: 10, fare: 230 },
+  { distance: 12, fare: 260 },
+  { distance: 15, fare: 320 }
+];
+
+// Simple ML-style prediction (average rate per km)
+function predictFare(distance) {
+  let totalDistance = 0;
+  let totalFare = 0;
+
+  for (let ride of pastRides) {
+    totalDistance += ride.distance;
+    totalFare += ride.fare;
+  }
+
+  const ratePerKm = totalFare / totalDistance;
+  const predictedFare = distance * ratePerKm;
+
+  return Math.round(predictedFare);
+}
+
+module.exports = predictFare;
