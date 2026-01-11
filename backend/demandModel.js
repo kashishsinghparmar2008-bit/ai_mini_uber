3f5a5b7b629f07b4ca6048f1cb8bc4fe3dc8afd1
kashishsinghparmar2008-bit <kashishsinghparmar2008@gmail.com>
2026-01-12 00:14:51 +0530

    Update demandModel.js

--- backend/demandModel.js
+++ backend/demandModel.js
@@ -1,1 +1,38 @@
+// backend/demandModel.js
+
+const rideHistory = [];
+
+function addRide(pickup) {
+  rideHistory.push(pickup);
+}
+
+function getHighDemandArea() {
+  const count = {};
+
+  for (let area of rideHistory) {
+    count[area] = (count[area] || 0) + 1;
+  }
+
+  let maxArea = null;
+  let maxCount = 0;
+
+  for (let area in count) {
+    if (count[area] > maxCount) {
+      maxCount = count[area];
+      maxArea = area;
+    }
+  }
+
+  return maxArea;
+}
+
+function getHeatmapData() {
+  return rideHistory;
+}
+
+module.exports = {
+  addRide,
+  getHighDemandArea,
+  getHeatmapData
+};
 
